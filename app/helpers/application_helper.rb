40a69cdac49b282fb72339ceb9edb5b3489ee917
Voight-Kampff <valfells@gmail.com>
2014-09-09 00:56:56 +0000

    added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
 	def active?(path)
 		"active" if current_page?(path)
 	end
+
+	def randomized_background_image
+  	images = ["assets/bg1.jpg", "assets/bg2.jpg"]
+  	images[rand(images.size)]
+	end
+
 end
